72752f6a398d0d550fad2326a6c69d86355af1cb
Tsunekawa Mao <tsunekaw@slis.tsukuba.ac.jp>
2014-10-24 21:49:33 +0900

    /v1/isbn/(isbn) returns a pair of isbn and ndc9 as json data

--- app.rb
+++ app.rb
@@ -6,5 +6,17 @@ class NDC9App < Sinatra::Base
   get '/' do
     'hello'
   end
+
+  get '/v1/isbn/:isbn' do
+    isbn = params[:isbn]
+    ns   = NDLSearch::NDLSearch.new
+    ndc9 = ns.search(:isbn=>isbn).items.first.ndc
+
+    content_type 'application/json'
+    {
+      :isbn => isbn,
+      :ndc9 => ndc9.to_s
+    }.to_json
+  end
   
 end
