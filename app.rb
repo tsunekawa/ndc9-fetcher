7048db44d6a59bbcc7525e887fc40eb78fa138f7
Tsunekawa Mao <tsunekaw@slis.tsukuba.ac.jp>
2014-10-24 22:23:32 +0900

    cache ndc9 with Redis

--- app.rb
+++ app.rb
@@ -1,6 +1,13 @@
 require 'bundler'
 Bundler.require
 
+if ENV["REDISTOGO_URL"] != nil
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  $redis = Redis.new host:"127.0.0.1", port:"6379"
+end
+
 class NDC9App < Sinatra::Base
 
   get '/' do
@@ -8,9 +15,16 @@ class NDC9App < Sinatra::Base
   end
 
   get '/v1/isbn/:isbn' do
-    isbn = params[:isbn]
-    ns   = NDLSearch::NDLSearch.new
-    ndc9 = ns.search(:isbn=>isbn).items.first.ndc
+    isbn =  params[:isbn].gsub("-","")
+    cache = params[:cache]=="true"
+
+    if cache and $redis.exists isbn then
+      ndc9 = $redis.get isbn
+    else
+      ns   = NDLSearch::NDLSearch.new
+      ndc9 = ns.search(:isbn=>isbn).items.first.ndc
+      $redis.set isbn, ndc9
+    end
 
     content_type 'application/json'
     {
