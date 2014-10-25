1c13cf4751d38535cd334bb396a4e0a28b5bcaa1
Tsunekawa Mao <tsunekaw@slis.tsukuba.ac.jp>
2014-10-26 01:44:22 +0900

    time interval of requesting to iss.ndl.go.jp keeps 1 sec

--- app.rb
+++ app.rb
@@ -10,9 +10,20 @@ else
 end
 
 module NDC9
+  def self.last_request
+    $redis.get "last_request"
+  end
+
   def self.fetch(isbn, opt={})
     cache = (opt["cache"] || true)
     self.cache(isbn, {:cache=>cache}) do
+        unless self.last_request.nil?
+          time = (Time.now - self.last_request)
+          if  tiem < 1.0 then
+            sleep (1.0-time)
+          end
+        end
+
         source = open("http://iss.ndl.go.jp/api/opensearch?dpid=iss-ndl-opac&isbn=#{isbn}&mediatype=1").read
         doc    = Nokogiri::XML.parse(source)
         doc.xpath("//channel/item/dc:subject[@xsi:type='dcndl:NDC9']/text()").to_s 
@@ -81,7 +92,8 @@ module NDC9
         value
       end
     else
-      yield
+      value = yield
+      $redis.set "last_request", Time.now
     end
   end
 end
