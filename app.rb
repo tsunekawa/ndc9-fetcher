e90c5995bd6dbfdafdb2b310fd4b99193ecbf696
Tsunekawa Mao <tsunekaw@slis.tsukuba.ac.jp>
2014-10-26 01:26:34 +0900

    add bulk mode

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require 'bundler'
 Bundler.require
-require 'sinatra/respond_with'
 
 # set up for Redis
 if ENV["REDISTOGO_URL"] != nil
@@ -8,6 +7,83 @@ if ENV["REDISTOGO_URL"] != nil
   $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else
   $redis = Redis.new host:"127.0.0.1", port:"6379"
+end
+
+module NDC9
+  def self.fetch(isbn, opt={})
+    cache = (opt["cache"] || true)
+    self.cache(isbn, {:cache=>cache}) do
+        source = open("http://iss.ndl.go.jp/api/opensearch?dpid=iss-ndl-opac&isbn=#{isbn}&mediatype=1").read
+        doc    = Nokogiri::XML.parse(source)
+        doc.xpath("//channel/item/dc:subject[@xsi:type='dcndl:NDC9']/text()").to_s 
+    end
+  end
+
+  def self.multi_fetch(isbn_list, opt={})
+    raise ArgumentError, "isbn_list should be Array" unless isbn_list.instance_of? Array
+    isbn_list.map do |isbn|
+      [isbn, self.fetch(isbn, opt)]
+    end
+  end
+
+  def self.bulk_request(isbn_list, opt={})
+    raise ArgumentError, "isbn_list should be Array" unless isbn_list.instance_of? Array
+    request_id = Digest::SHA1.hexdigest(isbn_list.to_s)
+
+    $redis.multi do
+      isbn_list.each do |isbn|
+        $redis.lpush "request:#{request_id}", isbn
+      end
+    end
+    request_id
+  end
+
+  def self.bulk_request_exists?(request_id)
+    $redis.exists("request:#{request_id}") or $redis.exists("result:#{request_id}")
+  end
+
+  def self.bulk_fetch(request_id)
+    key = "request:#{request_id}"
+    raise unless $redis.exists key
+
+    while $redis.llen(key) > 0 do
+      isbn = $redis.lindex(key, 0)
+      ndc9 = self.fetch(isbn)
+      $redis.multi do
+        $redis.lpop key
+        $redis.hset "result:#{request_id}", isbn, ndc9
+      end
+    end
+
+    $redis.expire "result:#{request_id}", 43200 # 6 hours
+    $redis.del "request:#{request_id}"
+
+    true
+  end
+
+  def self.bulk_get(request_id)
+    if !($redis.exists "request:#{request_id}") and $redis.exists "result:#{request_id}"
+      $redis.hgetall "result:#{request_id}"
+    else
+      nil
+    end
+  end
+
+  # cache and return value with Redis
+  def self.cache(name, opts={}, &block)
+    cache = opts[:cache]
+    if cache then
+      if $redis.exists name then
+        $redis.get name
+      else
+        value = yield
+        $redis.set name, value
+        value
+      end
+    else
+      yield
+    end
+  end
 end
 
 class NDC9App < Sinatra::Base
@@ -40,16 +116,16 @@ class NDC9App < Sinatra::Base
     erb :index
   end
 
+  get '/bulk' do
+    erb :bulk
+  end
+
   # return a NDC9 code number which correspond to the ISBN
   get '/v1/isbn/:isbn' do
     isbn =  params[:isbn].gsub("-","")
     cache = (params[:cache] || "true")=="true"
 
-    ndc9 = cache(isbn, {:cache=>cache}) do
-      source = open("http://iss.ndl.go.jp/api/opensearch?dpid=iss-ndl-opac&isbn=#{isbn}&mediatype=1").read
-      doc    = Nokogiri::XML.parse(source)
-      doc.xpath("//channel/item/dc:subject[@xsi:type='dcndl:NDC9']/text()").to_s 
-    end
+    ndc9 = NDC9.fetch(isbn, {:cache=>cache})
 
     respond_to do |f|
       f.html { ndc9.to_s }
@@ -60,26 +136,48 @@ class NDC9App < Sinatra::Base
     end
   end
 
+  post '/v1/isbn/bulk' do
+    cache = (params[:cache] || "true")=="true"
+
+    case request.media_type
+    when 'application/json'
+      data = JSON.parse request.body.read
+    when 'text/plain'
+      data = {"isbn"=>request.body.split("\n")}
+    when 'application/x-www-form-urlencoded'
+      data = {"isbn"=>params["isbn"].gsub("\r","").split("\n")}
+    else
+      error 406
+    end
+
+    request_id = NDC9.bulk_request(data["isbn"], {:cache=>cache})
+
+    EM.defer do
+      NDC9.bulk_fetch(request_id)
+    end
+
+    redirect "/v1/isbn/bulk/#{request_id}"
+  end
+
+  get '/v1/isbn/bulk/:request_id' do
+    halt 404 unless NDC9.bulk_request_exists? params["request_id"]
+
+    result = NDC9.bulk_get(params["request_id"])
+    if result.nil? then
+      status 102 # Processing
+      "Please wait...now processing"
+    else
+      status 200 # OK
+      respond_to do |f|
+        f.json { result.to_json }
+        f.txt  { result.to_a.unshift(["isbn", "ndc9"]).map{|i| i.join("\t")}.join("\n") }
+      end
+    end
+  end
+
   helpers do
     def root_path
       "#{request.scheme}://#{request.host_with_port}/"
     end
-
-    # cache and return value with Redis
-    def cache(name, opts={}, &block)
-      cache = opts[:cache]
-      if cache then
-        if $redis.exists name then
-          $redis.get name
-        else
-          value = yield
-          $redis.set name, value
-          value
-        end
-      else
-        yield
-      end
-    end
   end
 end
-
